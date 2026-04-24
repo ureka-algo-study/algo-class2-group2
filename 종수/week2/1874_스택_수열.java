366fe01da377b14c0ba3a29c4f5a644871bdc6d8
Seok_Soo <whdtn7855@naver.com>
2026-04-24 17:46:29 +0900

    feat: 1874_스택수열 문제 추가

---
+++ 종수/week2/1874_스택_수열.java
@@ -0,0 +1,13 @@
+package 종수.week2;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+    }//psvm
+
+    
+}//class
