ff53347b940952f50572a2c6d16ca910f29ff0b6
김가원(Kimgawon) <202021000@sangmyung.kr>
2026-05-13 18:10:40 +0900

    docs: 문제풀이 주석 추가

--- 가원/week4/12978_배달.py
+++ 가원/week4/12978_배달.py
@@ -8,11 +8,15 @@ from typing import List
 def solution(N: int, road: List[List[int]], K: int) -> int:
     answer: int = 0
 
+    # 그래프 초기화
     graph: List[List[int]] = [[] for _ in range(N + 1)]
 
+    # STEP1. 그래프 구성 -> 인접 리스트로 표현
     for a, b, cost in road:
         graph[a].append((b, cost))
         graph[b].append((a, cost))
+    
+    print(graph)
 
     distance = [float("inf")] * (N + 1)
     distance[1] = 0
@@ -23,7 +27,9 @@ def solution(N: int, road: List[List[int]], K: int) -> int:
     # 시작 마을 넣기 -> 1번 마을까지 가는 거리는 0이다.
     heapq.heappush(q, (0, 1))
 
+    # 우선순위 큐가 빌 때까지 반복
     while q:
+        # 가장 가까운 마을 꺼내기
         dist, now = heapq.heappop(q)
         # -> 가장 가까운 마을 꺼내기
         # 첫 단계로 우선순위 큐에 시작점 넣기
