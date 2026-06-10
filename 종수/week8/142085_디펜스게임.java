2aea044a3d2d9a8a2442cb1ddd2410b0b3265e32
Seok_Soo <whdtn7855@naver.com>
2026-06-10 15:43:57 +0900

    feat: 142085_디펜스게임 문제 풀이 추가

---
+++ 종수/week8/142085_디펜스게임.java
@@ -0,0 +1,53 @@
+package 프로그래머스;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+
+        PriorityQueue<Integer> round = new PriorityQueue<>(Collections.reverseOrder()); // 라운드 적? // 큰 수부터
+        PriorityQueue<Integer> unkillabledemonking = new PriorityQueue<>(); // 무적권 // 작은 수 부터
+
+        //
+        int idx = k; // 현재 진행하는 라운드
+
+        if (k >= enemy.length) {
+            return enemy.length;
+        }
+
+        for (int i = 0; i < k; i++) {
+            unkillabledemonking.add(enemy[i]);
+        }
+
+        while (idx < enemy.length) {
+
+            round.add(enemy[idx]);
+            n -= enemy[idx];
+
+            if (round.peek() > unkillabledemonking.peek()) {
+                int temp = round.poll();
+                n += temp;
+                n -= unkillabledemonking.peek();
+                round.add(unkillabledemonking.poll());
+                unkillabledemonking.add(temp);
+            }
+
+            if (n < 0)
+                break;
+
+            idx++;
+
+        }
+
+        answer = idx;
+
+        return answer;
+    }
+}
+
+// n명의 병사를 가지고 시작
+// enemy.length 는 진행할 라운드
+// enemy.[i]는 라운드별 등장하는 적
+// 한라운드를 적의 숫자와 관계없이 넘길 수 있는 무적권이 k개
