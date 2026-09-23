97f6687f58f060a67de9b06d3bd18ce1884c68cf
alsthdua107 <alsthdua107@gmail.com>
2026-09-23 18:01:09 +0900

    week22_실패 코드 추가

---
+++ 민주/week22/86052_빛의경로사이클_fail.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+class Solution {
+    boolean[][][] visited;
+    int N;
+    int M;
+    String[] grid;
+    ArrayList<Integer> ans = new ArrayList<>();
+    
+    int[] di = {0,0,-1,1}; // L R U D
+    int[] dj= {-1,1,0,0}; // L R U D
+    
+    public int[] solution(String[] grid) {
+        int[] answer = {};
+        N = grid.length;
+        M = grid[0].length();
+        visited = new boolean[N][M][4];
+        this.grid = grid;
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                for(int d=0; d<4; d++){
+                    dfs(i,j,d,0);
+                }
+            }
+        }
+        answer = ans.stream().mapToInt(i->i).toArray();
+        Arrays.sort(answer);
+        return answer;
+    }
+    
+    void dfs(int x, int y, int dir, int cnt){
+        if(visited[x][y][dir]){
+            if(cnt != 0){
+                ans.add(cnt);
+            }
+            return;
+        }
+        
+        visited[x][y][dir] = true;
+        
+        int nx = x;
+        int ny = y;
+        if(grid[x].charAt(y) == 'L'){
+            // L R U D
+            // L(0) -> U(2), R(1) -> D(3), U(2) -> R(1), D(3) -> L(0)
+            if(dir <= 1){
+                dir = (dir+2) % 4;
+            }
+            else{
+                dir = (3 - dir);
+            }
+            
+        }
+        else if(grid[x].charAt(y) == 'R'){
+            // L R U D
+            // L(0) -> D(3), R(1) -> U(2), U(2) -> L(0), D(3) -> R(1)
+            if(dir <= 1){
+                dir = 3 - dir;
+            }
+            else{
+                dir = (dir-2);
+            }
+        }
+        
+        nx += di[dir];
+        ny += dj[dir];
+        
+        if(nx < 0) {
+            nx = N-1;
+        }
+        else if(nx >= N){
+            nx = 0;
+        }
+        if(ny < 0){
+            ny = M-1;
+        }
+        else if(ny >= M){
+            ny = 0;
+        }
+        
+        dfs(nx, ny, dir, cnt+1);
+    }
+}
